b2be72f46769c10cce91920e08f186ac899070a5
AYAN CHOUDHURI <ayan.choudhuri.86@gmail.com>
2020-07-07 02:03:05 +0530

    First Check in

---
+++ src/com/ayan/project/test/Test.java
@@ -0,0 +1,11 @@
+package com.ayan.project.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String fName = "Ayan;";
+		String lName = "Choudhuri";
+		System.out.println("Hello " + fName + " " + lName);
+	}
+
+}
